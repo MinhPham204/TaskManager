5a35b507168d83d2ca806272cd805eb3cc400f69
MinhPham204 <pnhatminh2004@gmail.com>
2025-10-14 02:10:16 +0700

    rm

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const redis = require("redis");
 
-// Create client with endpoint and password from redis
 const redisClient = redis.createClient({
   socket: {
-    host: "redis-15636.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com", 
-    port: 15636, 
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
   },
-  password: "0Q0sCyNZCt8FTUqsyFVRe6S185lXIrGn", 
+  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
